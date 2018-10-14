0601879610b61baf11ac3ceaeff69be5ff6a05e6
m0ksem <m0ksem1337@gmail.com>
2018-10-14 21:06:19 +0300

    Starting creating new type

---
+++ app/src/main/java/com/example/m0ksem/imprtest/Results/OneStringResult.kt
@@ -0,0 +1,12 @@
+package com.example.m0ksem.imprtest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GetResult : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_get_result)
+    }
+}
