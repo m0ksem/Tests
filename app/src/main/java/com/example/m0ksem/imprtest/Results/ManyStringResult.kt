dde3229ead593d9199f696a21ddb1742cd9e234d
m0ksem <m0ksem1337@gmail.com>
2018-10-22 08:47:04 +0300

    Created assotiation test and SQLite DB

---
+++ app/src/main/java/com/example/m0ksem/imprtest/Results/ManyStringResult.kt
@@ -0,0 +1,22 @@
+package com.example.m0ksem.imprtest.Results
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.example.m0ksem.imprtest.R
+
+@Suppress("UNUSED_PARAMETER")
+class OneStringResult : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_get_one_result)
+        findViewById<TextView>(R.id.result).text = intent.getStringExtra("result")
+    }
+
+    fun back(view: View){
+        finish()
+    }
+
+}
